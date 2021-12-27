217bbae07f65799ed24029c0f918a634b926ed2e
chineidu <chineiduezeofor@gmail.com>
2021-12-27 14:04:19 +0100

    added tests for addition of users

--- src/api/users.py
+++ src/api/users.py
@@ -1,26 +1,45 @@
-from flask import Blueprint
-from flask_restx import Resource, Api, reqparse
+from flask import Blueprint, current_app
+from flask_restx import Resource, Api, reqparse, fields
 from src import db
 from .models import Users
+from src.status import status_code
 
 
 users_bp = Blueprint("Users", __name__)
 api = Api(users_bp)
 
+users_model = api.model(
+    "User",
+    {
+        "id": fields.Integer(readOnly=True),
+        "username": fields.String(required=True),
+        "email": fields.String(required=True),
+        "created_at": fields.DateTime,
+    },
+)
+
 parser = reqparse.RequestParser()
-parser.add_argument("username", required=True, type=str, help="Enter the username ")
-parser.add_argument("email", required=True, type=str, help="Enter the email ")
+parser.add_argument("username", type=str, help="Enter the username ")
+parser.add_argument("email", type=str, help="Enter the email ")
 
 
 @api.route("/users")
 class UsersList(Resource):
+    @api.expect(users_model, validate=True)
     def post(self):
         data = parser.parse_args()
         username = data.get("username")
         email = data.get("email")
 
+        user = Users.query.filter_by(email=email).first()
+        if user:
+            current_app.logger.debug("USER CREATION FAILED!")
+            response_object = {"message": "Sorry. That email already exists."}
+            return response_object, status_code.BAD_REQUEST_400
+
+        current_app.logger.info("USER SUCESSFULLY ADDED!")
         db.session.add(Users(username=username, email=email))
         db.session.commit()
 
         response_object = {"message": f"{email} was added!"}
-        return response_object, 201
+        return response_object, status_code.CREATED_201
--- src/tests/test_add_users.py
+++ src/tests/test_add_users.py
@@ -1,13 +1,70 @@
 import json
+from src.status import status_code
 
 
 def test_add_user(test_app, test_db):
+    """This is used to test the addition of a new user."""
+    # Given
     with test_app.test_client() as client:
+        
+        # When
         response = client.post(
             "/users",
             data=json.dumps({"username": "michael", "email": "michael@testdriven.io"}),
             content_type="application/json",
         )
         data = json.loads(response.data.decode("utf-8"))
-        assert response.status_code == 201
+        
+        # Then
+        assert response.status_code == status_code.CREATED_201
         assert "michael@testdriven.io was added!" in data.get("message")
+
+
+def test_add_user_invalid_json(test_app, test_db):
+    """This is used to test the addition of a user with an empty payload."""
+    # Given
+    with test_app.test_client() as client:
+
+        # When
+        response = client.post(
+            "/users", data=json.dumps({}), content_type="application/json"
+        )
+        data = json.loads(response.data.decode("utf-8"))
+        
+        # Then
+        assert response.status_code == status_code.BAD_REQUEST_400
+        assert "Input payload validation failed" in data.get("message")
+
+
+def test_add_user_invalid_json_keys(test_app, test_db):
+    """This is used to test the addition of a user with an invalid/incomplete keys"""
+    # Given
+    with test_app.test_client() as client:
+
+        # When
+        response = client.post(
+            "/users",
+            data=json.dumps({"email": "john@testdriven.io"}),
+            content_type="application/json",
+        )
+        data = json.loads(response.data.decode("utf-8"))
+
+        # Then
+        assert response.status_code == status_code.BAD_REQUEST_400
+        assert "Input payload validation failed" in data.get("message")
+
+
+def test_add_user_duplicate_email(test_app, test_db):
+    """This is used to test the addition of a user with an duplicate emails"""
+    # Given
+    with test_app.test_client() as client:
+        # When
+        response = client.post(
+            "/users",
+            data=json.dumps({"username": "michael", "email": "michael@testdriven.io"}),
+            content_type="application/json",
+        )
+        data = json.loads(response.data.decode("utf-8"))
+        # Then
+        assert response.status_code == status_code.BAD_REQUEST_400
+        assert "Sorry. That email already exists." in data.get("message")
