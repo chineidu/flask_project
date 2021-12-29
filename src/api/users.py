88e1ec3f5f8ffab8c7c8a42af265e689b7e6bc99
chineidu <chineiduezeofor@gmail.com>
2021-12-29 21:10:51 +0100

    added CRUD endpoints

--- src/api/users.py
+++ src/api/users.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, current_app
 from flask_restx import Resource, Api, reqparse, fields
 from src import db
-from .models import Users
+from src.api.models import Users
 from src.status import status_code
 
 
@@ -14,7 +14,7 @@ users_model = api.model(
         "id": fields.Integer(readOnly=True),
         "username": fields.String(required=True),
         "email": fields.String(required=True),
-        "created_at": fields.DateTime,
+        "created_date": fields.DateTime(readOnly=True),
     },
 )
 
@@ -23,8 +23,28 @@ parser.add_argument("username", type=str, help="Enter the username ")
 parser.add_argument("email", type=str, help="Enter the email ")
 
 
+@api.route("/users/<int:user_id>")
+class Users_(Resource):
+    @api.marshal_with(users_model)
+    def get(self, user_id: int):
+        user = Users.query.filter_by(id=user_id).first()
+        if not user:
+            current_app.logger.debug("USER RETRIEVAL FAILED!")
+            return api.abort(404, f"User {user_id} does not exist")
+
+        # else
+        current_app.logger.info("USER SUCCESSFULLY RETRIEVED!")
+        return user, status_code.OK_200
+
+
 @api.route("/users")
 class UsersList(Resource):
+    @api.marshal_with(users_model, as_list=True)
+    def get(self):
+        user = Users.query.all()
+        current_app.logger.info("ALL USERS SUCCESSFULLY RETRIEVED!")
+        return user, status_code.OK_200
+
     @api.expect(users_model, validate=True)
     def post(self):
         data = parser.parse_args()
